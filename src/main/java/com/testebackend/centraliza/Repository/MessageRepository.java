943976413693d330ca1f46b08959c6c0825f1e56
Nilton Cesar Barbosa dos Santos <niltonbarbosa032@gmail.com>
2026-01-21 19:10:14 -0300

    Alteração da MessageRepository adicionado duas novas consultas

--- src/main/java/com/testebackend/centraliza/Repository/MessageRepository.java
+++ src/main/java/com/testebackend/centraliza/Repository/MessageRepository.java
@@ -2,6 +2,24 @@ package com.testebackend.centraliza.Repository;
 
 import com.testebackend.centraliza.Entity.MessageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface MessageRepository extends JpaRepository<MessageEntity, Long> {
+    //Usei para estudar o @Query e estudei o Derived Query
+    //Nos estudos o que mais senti familiaridade foi o @Query, uma que consigo ter mais controle e outra que visualmente fica mais visível a interpretação
+    @Query(value = "SELECT * FROM message WHERE id_conversation_fk = :idConversation ORDER BY timestamp ASC",
+            nativeQuery = true)
+    List<MessageEntity> findMessagesByConversation(@Param("idConversation") Long idConversation);
+
+    @Query(value = "SELECT * FROM message WHERE id_conversation_fk = :idConversation ORDER BY timestamp DESC LIMIT 1",
+            nativeQuery = true)
+    Optional<MessageEntity> findLastMessage(@Param("idConversation") Long idConversation);
+
+    //Deixei comentado para mostrar como ficou
+    //List<MessageEntity> findByConversationEntityIdOrderByTimestampAsc(Long idConversation);
+    //Optional<MessageEntity> findFirstByConversationEntityIdOrderByTimestampDesc(Long idConversation);
 }
