595f74c88a22354d744e101dda00665ec505ec91
Nilton Cesar Barbosa dos Santos <niltonbarbosa032@gmail.com>
2026-01-22 08:01:36 -0300

    Criar dados do user no banco automatimente

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name_user) VALUES ('Nilton Santos');
+INSERT INTO users (name_user) VALUES ('Gustavo Santos');
+INSERT INTO users (name_user) VALUES ('Pedro Oliveira');
+INSERT INTO users (name_user) VALUES ('João Costa');
+INSERT INTO users (name_user) VALUES ('Carlos Souza');
